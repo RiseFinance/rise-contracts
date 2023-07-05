e6d146859687b3ffb436d1dc9682d04a9d325756
Junha <vjunhav@gmail.com>
2023-07-05 14:59:24 +0900

    L3Vault contract created

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,6 +3,12 @@ import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.18",
+  networks: {
+    l3local: {
+      url: "http://localhost:8449", // Local Arbitrum L3 node
+      accounts: [process.env.DEPLOY_PRIVATE_KEY as string], // 0xDe264e2133963c9f40e07f290E1D852f7e4e4c7c
+    },
+  },
 };
 
 export default config;
