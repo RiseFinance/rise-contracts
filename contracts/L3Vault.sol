b866dc375e5491b082e17e4d0839d761d6480af8
Junha <vjunhav@gmail.com>
2023-07-05 16:18:26 +0900

    feat: sample openPosition function impl

--- contracts/L3Vault.sol
+++ contracts/L3Vault.sol
@@ -2,6 +2,8 @@
 
 pragma solidity ^0.8.0;
 
+import "./interfaces/IPriceFeed.sol";
+
 contract L3Vault {
     struct UserVault {
         uint256 balance;
@@ -27,14 +29,14 @@ contract L3Vault {
 
     struct Position {
         uint256 size;
-        uint256 collateral;
+        uint256 collateralSize;
         uint256 averagePrice;
-        uint256 reserveAmount;
         int256 realizedPnl;
         uint256 lastUpdatedTime;
     }
 
     // mapping to MerkleTree
+    IPriceFeed public priceFeed;
     mapping(address => mapping(uint256 => UserVault)) public traderBalances; // userKey => assetId => UserVault
     mapping(address => mapping(uint256 => Order)) public traderOrders; // userKey => orderId => Order
 
@@ -45,4 +47,85 @@ contract L3Vault {
     mapping(uint256 => GlobalPositionState) public globalShortStates;
 
     mapping(bytes32 => Position) public positions; // positionHash => Position
+
+    function getPositionKey(
+        address _account,
+        uint256 _collateralAssetId,
+        uint256 _indexAssetId,
+        bool _isLong
+    ) public pure returns (bytes32) {
+        return
+            keccak256(
+                abi.encodePacked(
+                    _account,
+                    _collateralAssetId,
+                    _indexAssetId,
+                    _isLong
+                )
+            );
+    }
+
+    function getMarkPrice(uint256 assetId) internal view returns (uint256) {
+        // to implement in customized ArbOS
+        return priceFeed.getPrice(assetId);
+    }
+
+    function getPoolAmount(uint256 assetId) public view returns (uint256) {
+        return tokenPoolAmounts[assetId];
+    }
+
+    function getReserveAmount(uint256 assetId) public view returns (uint256) {
+        return tokenReserveAmounts[assetId];
+    }
+
+    function addLiquidity(uint256 assetId, uint256 amount) external {
+        // liquidity 기록을 변수에 할지, LP 토큰을 발행할지 결정
+        // 발행한다면 모든 settlement chain에? (e.g. Arbitrum, zkSync, etc.)
+        tokenPoolAmounts[assetId] += amount;
+    }
+
+    function removeLiquidity(uint256 assetId, uint256 amount) external {
+        tokenPoolAmounts[assetId] -= amount;
+    }
+
+    // open Position
+    function openPosition(
+        address _account,
+        uint256 _collateralAssetId,
+        uint256 _indexAssetId,
+        uint256 _size,
+        uint256 _collateralSize,
+        bool _isLong
+    ) external returns (bool) {
+        // create Order
+        // update Position
+        bytes32 key = getPositionKey(
+            _account,
+            _collateralAssetId,
+            _indexAssetId,
+            _isLong
+        );
+
+        Position storage position = positions[key];
+        require(position.size == 0, "L3Vault: position already exists");
+        uint256 markPrice = getMarkPrice(_indexAssetId);
+
+        // update position fields
+        position.size = _size;
+        position.collateralSize = _collateralSize;
+        position.averagePrice = markPrice;
+        position.realizedPnl = 0;
+        position.lastUpdatedTime = block.timestamp;
+
+        // update GlobalPositionState
+        // update UserVault
+        UserVault storage userVault = traderBalances[_account][_indexAssetId];
+        userVault.balance -= _collateralSize;
+
+        // update tokenReserveAmounts
+        tokenReserveAmounts[_indexAssetId] += _size;
+        // update traderOrders
+        return true;
+    }
+    // close Position
 }
