d9103a94238d97285a8654a1d1c4ff6d644653eb
Junha <vjunhav@gmail.com>
2023-07-08 20:19:18 +0900

    update: deployement script updated

--- scripts/deploy_l3vault.ts
+++ scripts/deploy_l3vault.ts
@@ -9,14 +9,22 @@ async function main() {
   console.log("Account balance:", (await deployer.getBalance()).toString());
 
   // ==================== Set Contract Factory & Constructor ====================
+  const PriceFeedContract = await ethers.getContractFactory("PriceFeed");
+  const L3VaultContract = await ethers.getContractFactory("L3Vault");
+  console.log(">>> got contract factories");
 
-  const Contract = await ethers.getContractFactory("L3Vault");
-  console.log(">>> got contract factory");
-  const contract = await Contract.deploy();
-  // ============================================================================
-  console.log(">>> Deployment in progress...");
-  await contract.deployed();
-  console.log(`Deployed Contract: ${contract.address}`);
+  // ==================== Deploy Contracts ====================
+  const priceFeedContract = await PriceFeedContract.deploy();
+  console.log(">>> PriceFeed Deployment in progress...");
+  await priceFeedContract.deployed();
+  console.log(`Deployed PriceFeed Contract: ${priceFeedContract.address}`);
+
+  const l3VaultContract = await L3VaultContract.deploy(
+    priceFeedContract.address
+  );
+  console.log("\n>>> L3Vault Deployment in progress...");
+  await l3VaultContract.deployed();
+  console.log(`Deployed L3Vault Contract: ${l3VaultContract.address}`);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
