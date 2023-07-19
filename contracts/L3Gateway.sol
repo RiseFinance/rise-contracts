c3824187d1e6d1372bbae03f5f04553ad8aea791
Junha <vjunhav@gmail.com>
2023-07-19 13:11:25 +0900

    feat: L2Gateway & L3Gateway ETH withdrawal functions

--- contracts/L3Gateway.sol
+++ contracts/L3Gateway.sol
@@ -2,13 +2,18 @@
 
 pragma solidity ^0.8.0;
 
+import "./common/Context.sol";
 import "./interfaces/IL3Vault.sol";
+import "../l2-contracts/interfaces/IL2Gateway.sol";
+import {ArbSys} from "./interfaces/ArbSys.sol";
 
-contract L3Gateway {
+contract L3Gateway is Context {
+    address public l2GateawayAddress;
     IL3Vault public l3Vault;
 
-    constructor(address _l3Vault) {
+    constructor(address _l3Vault, address _l2Gateway) {
         l3Vault = IL3Vault(_l3Vault);
+        l2GateawayAddress = _l2Gateway;
     }
 
     function increaseTraderBalance(
@@ -21,5 +26,22 @@ contract L3Gateway {
 
     function addLiquidity(uint256 _assetId, uint256 _amount) external {
         l3Vault.addLiquidity(_assetId, _amount);
+    }
+
+    // -------------------- L3 -> L2 Messaging --------------------
+
+    // TODO: L3 gas fee should be paid by the L2 user (or by L3 admin contract)
+    function withdrawEthToL2(uint256 _amount) external {
+        uint256 balance = l3Vault.getTraderBalance(msg.sender, ETH_ID);
+        require(balance >= _amount, "L3Gateway: insufficient balance");
+
+        l3Vault.decreaseTraderBalance(msg.sender, ETH_ID, _amount);
+
+        bytes memory data = abi.encodeWithSelector(
+            IL2Gateway.withdrawEthFromOutbox.selector,
+            msg.sender, // _dest => not allowing to designate a different recipient address
+            _amount // _amount
+        );
+        ArbSys(address(100)).sendTxToL1(l2GateawayAddress, data);
     }
 }
--- l2-contracts/L2Gateway.sol
+++ l2-contracts/L2Gateway.sol
@@ -4,8 +4,18 @@ pragma solidity ^0.8.0;
 
 import "./interfaces/IInbox.sol";
 import "../contracts/interfaces/IL3Gateway.sol";
+import "../contracts/GatewayHelper.sol";
 
-contract L2Gateway {
+contract L2Gateway is GatewayHelper {
+    error NotBridge(address sender); // TODO: move to errors
+
+    struct InOutInfo {
+        uint256 index;
+        bool allowed;
+    }
+
+    mapping(address => InOutInfo) private allowedBridgesMap;
+
     address public l3GatewayAddress;
     IInbox public inbox;
 
@@ -26,17 +36,16 @@ contract L2Gateway {
     }
 
     /**
-     * @param depositAmount amount of ETH to deposit
-     * @param maxSubmissionCost the maximum amount of ETH to be paid for submitting the retryable ticket
-     * @param gasLimit the maximum amount of gas used to cover L3 execution of the ticket
-     * @param gasPriceBid the gas price bid for L3 execution of the ticket
-     *
-     * @return ticketId the unique id of the retryable ticket created
-     *
      * @dev `msg.value` should include
      * 1) the ETH deposit amount for the Rise Finance L3 application
      * 2) cost of submitting and executing the retryable ticket, which is `l3CallValue + maxSubmissionCost + gasLimit * gasPriceBid`
      *    here, l3CallValue is always zero (not allowing ETH or ERC-20 token representations of traders balances to be minted in L3)
+     *
+     * @param depositAmount amount of ETH to deposit
+     * @param maxSubmissionCost the maximum amount of ETH to be paid for submitting the retryable ticket
+     * @param gasLimit the maximum amount of gas used to cover L3 execution of the ticket
+     * @param gasPriceBid the gas price bid for L3 execution of the ticket
+     * @return ticketId the unique id of the retryable ticket created
      */
     function depositEthToL3(
         uint256 depositAmount,
@@ -61,27 +70,48 @@ contract L2Gateway {
             depositAmount // _amount
         );
 
-        // TODO: decide where the locked tokens should be sent (L2Gateway or Inbox?)
+        // with no custom ArbSys withdraw function, the deposit amount must be held in L2Gateway
+        // and only Ticket process fees would be sent to the Bridge via Inbox
         uint256 ticketId = inbox.createRetryableTicket{
             value: maxSubmissionCost + gasLimit * gasPriceBid
         }(
             l3GatewayAddress,
             0, // l3CallValue
             maxSubmissionCost,
-            msg.sender, // excessFeeRefundAddress // FIXME: which account should deal with the gas fee?
-            msg.sender, // callValueRefundAddress // FIXME: which account should deal with the gas fee?
+            msg.sender, // excessFeeRefundAddress // TODO: aggregate excess fees on a L3 admin contract (not msg.sender)
+            msg.sender, // callValueRefundAddress
             gasLimit,
             gasPriceBid,
             data
         );
 
         // refund excess ETH
-        payable(msg.sender).transfer(
-            msg.value - (maxSubmissionCost + gasLimit * gasPriceBid)
+        _transferEth(
+            payable(msg.sender),
+            msg.value -
+                (depositAmount + maxSubmissionCost + gasLimit * gasPriceBid)
         );
-        // TODO: excess gas fee
+        // TODO: refund excess gas fee after processing the ticket
 
         // emit RetryableTicketCreated(ticketId);
         return ticketId;
+    }
+
+    /**
+     * @notice restricted to be called by the allowed L2 Bridges
+     */
+    function withdrawEthFromOutbox(
+        address _recipient,
+        uint256 _amount
+    ) external {
+        // call: L3 ArbSys.sendTxToL1 => Oubox.executeTransaction => Bridge.executeCall => L2Gateway.withdrawEthFromOutbox
+        // Not allowed to be directly called
+
+        // require(tx.origin == _recipient); // cannot delegate the execution to keepers with this condition
+
+        if (!allowedBridgesMap[msg.sender].allowed)
+            revert NotBridge(msg.sender);
+
+        _transferEth(payable(_recipient), _amount);
     }
 }
