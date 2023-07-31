87e820262c8adf8d52c353a8c0b96fec2269621b
Junha <vjunhav@gmail.com>
2023-07-31 14:45:09 +0900

    feat: Market, MarketVault, MarketFactory contracts

--- contracts/market/MarketFactory.sol
+++ contracts/market/MarketFactory.sol
@@ -3,6 +3,7 @@
 pragma solidity ^0.8.0;
 
 import "../token/RMM.sol";
+import "./Market.sol";
 
 // Deals with listing new markets and updating existing ones
 // * Listing new markets
@@ -11,5 +12,31 @@ import "../token/RMM.sol";
 contract MarketFactory {
     // TODO: create LP token contract for each market
 
-    RMM rmm = new RMM{}();
+    function createRisePerpsMarket(
+        Market.MarketInfo memory m
+    ) public returns (Market.MarketInfo memory) {
+        bytes32 salt = keccak256(
+            abi.encode(
+                "RISE_PERPS_MARKET",
+                m.marketId,
+                m.baseAssetId,
+                m.quoteAssetId
+            )
+        );
+
+        RiseMarketMaker rmm = new RiseMarketMaker{salt: salt}(); // market maker token
+
+        Market.MarketInfo memory newMarket = Market.MarketInfo(
+            m.marketId,
+            m.priceTickSize,
+            m.baseAssetId,
+            m.quoteAssetId,
+            m.longReserveAssetId,
+            m.shortReserveAssetId,
+            m.marginAssetId,
+            address(rmm)
+        );
+
+        return newMarket;
+    }
 }
--- contracts/market/MarketVault.sol
+++ contracts/market/MarketVault.sol
@@ -2,4 +2,40 @@
 
 pragma solidity ^0.8.0;
 
-contract MarketVault {}
+import "./Market.sol";
+
+contract MarketVault {
+    mapping(uint256 => Market.MarketInfo) public markets; // marketId => MarketInfo
+    uint256 public globalMarketIdCounter = 0;
+
+    function getMarketInfo(
+        uint256 _marketId
+    ) external view returns (Market.MarketInfo memory) {
+        return markets[_marketId];
+    }
+
+    function getMarketIdCounter() external view returns (uint256) {
+        return globalMarketIdCounter;
+    }
+
+    function getPriceTickSize(
+        uint256 _marketId
+    ) external view returns (uint256) {
+        Market.MarketInfo memory marketInfo = markets[_marketId];
+        require(
+            marketInfo.priceTickSize != 0,
+            "MarketVault: priceTickSize not set"
+        );
+        return marketInfo.priceTickSize;
+    }
+
+    function setPriceTickSize(
+        uint256 _marketId,
+        uint256 _tickSizeInUsd
+    ) public {
+        // TODO: only owner
+        // TODO: event - shows the previous tick size
+        Market.MarketInfo storage marketInfo = markets[_marketId];
+        marketInfo.priceTickSize = _tickSizeInUsd;
+    }
+}
