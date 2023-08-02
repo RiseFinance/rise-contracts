04e4f5e66b8ec62d57f2a830bd222e4accaa5299
윤재 <yoonjae@yunjaeui-MacBookPro.local>
2023-08-02 12:57:15 +0900

    Update: liquidation.sol code update

--- contracts/liquidation/Liquidation.sol
+++ contracts/liquidation/Liquidation.sol
@@ -2,10 +2,14 @@
 
 pragma solidity ^0.8.0;
 
+import "@openzeppelin/contracts/utils/math/Math.sol";
+
 import "../common/constants.sol";
 import "../oracle/PriceManager.sol";
 import "../market/TokenInfo.sol";
 import "../market/Market.sol";
+import "../common/MathUtils.sol";
+import "../account/TraderVault.sol";
 
 contract Liquidation {
     mapping(uint256 => uint256) maintenanceMarginRatioInBasisPoints; // assetId => maintenanceMarginRatio
@@ -14,20 +18,121 @@ contract Liquidation {
     PriceManager public priceManager;
     TokenInfo public tokenInfo;
     Market public market;
+    TraderVault public traderVault;
 
-    function executeLiquidationIfNeeded(
-        Position calldata _position,
-        uint256 _walletBalance
+    function mulDiv(
+        uint256 x,
+        uint256 y,
+        uint256 denominator
+    ) internal pure returns (uint256) {
+        return Math.mulDiv(x, y, denominator);
+    }
+
+    function executeLiquidations(
+        Position[] calldata _positions,
+        address[] calldata _traders
     ) external {
-        if (shouldLiquidate(_position, _walletBalance)) {
-            _executeLiquidation();
+        for (uint256 i = 0; i < _positions.length; i++) {
+            liquidatePosition(_positions[i]);
+        }
+        for (uint256 i = 0; i < _traders.length; i++) {
+            liquidateTrader(_traders[i]);
         }
     }
 
-    function _executeLiquidation() internal {
-        // Close all positions
+    function liquidatePosition(Position calldata _position) internal {
+        if (!_isPositionLiquidationValid(_position)) return;
+        // TODO: liquidate Position
     }
 
+    function liquidateTrader(address _trader) internal {
+        if (!_isTraderLiquidationValid(_trader)) return;
+        // TODO: liquidate all positions of trader
+    }
+
+    function _isPositionLiquidationValid(
+        Position calldata _position
+    ) internal returns (bool) {
+        uint256 baseAssetId = market
+            .getMarketInfo(_position.marketId)
+            .baseAssetId;
+        uint256 tokenPrecision = 10 ** tokenInfo.getTokenDecimals(baseAssetId);
+        (uint256 lefthandSide, uint256 righthandSide) = _calculateFormula(
+            priceManager.getMarkPrice(baseAssetId),
+            priceManager.getIndexPrice(baseAssetId),
+            _position.avgOpenPrice,
+            _position.size,
+            _position.isLong,
+            maintenanceMarginRatioInBasisPoints[baseAssetId],
+            PRICE_BUFFER_DELTA_TO_SIZE,
+            tokenPrecision,
+            BASIS_POINTS
+        );
+        return _position.margin + lefthandSide < righthandSide;
+    }
+
+    function _isTraderLiquidationValid(
+        address _trader
+    ) internal returns (bool) {
+        // TODO : getWalletBalance 구현 필요
+        // uint256 lefthandSide = traderVault.getWalletBalance(_trader);
+        uint256 lefthandSide = 0; // 임시
+        uint256 righthandSide = 0;
+        // TODO : getTraderPositions 구현 필요
+        // Position[] memory positions = traderVault.getTraderPositions(_trader);
+        Position[] memory positions; // 임시
+        for (uint256 i = 0; i < positions.length; i++) {
+            uint256 baseAssetId = market
+                .getMarketInfo(positions[i].marketId)
+                .baseAssetId;
+            (
+                uint256 lefthandSideDelta,
+                uint256 righthandSideDelta
+            ) = _calculateFormula(
+                    priceManager.getMarkPrice(baseAssetId),
+                    priceManager.getIndexPrice(baseAssetId),
+                    positions[i].avgOpenPrice,
+                    positions[i].size,
+                    positions[i].isLong,
+                    maintenanceMarginRatioInBasisPoints[baseAssetId],
+                    PRICE_BUFFER_DELTA_TO_SIZE,
+                    10 ** tokenInfo.getTokenDecimals(baseAssetId),
+                    BASIS_POINTS
+                );
+            lefthandSide += lefthandSideDelta;
+            righthandSide += righthandSideDelta;
+        }
+        return lefthandSide < righthandSide;
+    }
+
+    function _calculateFormula(
+        uint256 p,
+        uint256 pi,
+        uint256 pe,
+        uint256 s,
+        bool isLong,
+        uint256 r,
+        uint256 c,
+        uint256 SP,
+        uint256 RP
+    ) internal pure returns (uint256, uint256) {
+        uint256 p1 = isLong ? p : pe;
+        uint256 p2 = isLong ? pe : p;
+        uint256 lefthandSide = mulDiv(p1, s, SP);
+        uint256 righthandSide = mulDiv(p2, s, SP) +
+            mulDiv(mulDiv(p, s, SP), r, RP) +
+            mulDiv(mulDiv(pi, s, SP), s, SP) /
+            c /
+            2;
+        return (lefthandSide, righthandSide);
+    }
+
+    // function _executeLiquidation() internal {
+    //     // Close all positions
+    // }
+}
+
+/*
     // Only isolated mode
     // TODO: build for cross mode
     function getLiquidationPrice(
@@ -88,3 +193,4 @@ contract Liquidation {
         }
     }
 }
+*/
