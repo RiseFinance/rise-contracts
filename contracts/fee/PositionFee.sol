1c7d452da02dce077df5a9f13027d3650c1782fd
윤재 <yoonjae@yunjaeui-MacBookPro.local>
2023-08-03 17:14:16 +0900

    update : wrote positionfee code

---
+++ contracts/fee/PositionFee.sol
@@ -0,0 +1,50 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "../account/TraderVault.sol";
+
+contract PositionFee {
+    TraderVault public traderVault;
+
+    uint256 public constant MARKET_POSITION_FEE_CONSTANT = 5;
+    uint256 public constant LIMIT_POSITION_FEE_CONSTANT = 5;
+    uint256 public constant POSITION_FEE_PRECISION = 1e4;
+    uint256 public collectedPositionFees = 0;
+
+    // TODO : POSITION_FEE_CONSTANT , POSITION_FEE_PRECISION 값 확인 필요
+
+    function getLimitPositionFee(
+        uint256 _sizeAbs
+    ) public pure returns (uint256) {
+        return
+            (LIMIT_POSITION_FEE_CONSTANT * _sizeAbs) / POSITION_FEE_PRECISION;
+    }
+
+    function payLimitPositionFee(
+        address _trader,
+        uint256 _feeAssetId,
+        uint256 _sizeAbs
+    ) external {
+        uint256 fee = getLimitPositionFee(_sizeAbs);
+        traderVault.decreaseTraderBalance(_trader, _feeAssetId, fee);
+        collectedPositionFees += fee;
+    }
+
+    function getMarketPositionFee(
+        uint256 _sizeAbs
+    ) public pure returns (uint256) {
+        return
+            (MARKET_POSITION_FEE_CONSTANT * _sizeAbs) / POSITION_FEE_PRECISION;
+    }
+
+    function payMarketPositionFee(
+        address _trader,
+        uint256 _feeAssetId,
+        uint256 _sizeAbs
+    ) external {
+        uint256 fee = getMarketPositionFee(_sizeAbs);
+        traderVault.decreaseTraderBalance(_trader, _feeAssetId, fee);
+        collectedPositionFees += fee;
+    }
+}
