d803cf425329d516d87cba9942797c70a3d89324
Junha <vjunhav@gmail.com>
2023-08-10 12:00:02 +0900

    update: deploy scripts

--- scripts/deploy_l2.ts
+++ scripts/deploy_l2.ts
@@ -1,20 +1,6 @@
-import { ethers } from "hardhat";
 import { deployContract } from "../utils/deployer";
 
-async function main() {
-  const [deployer] = await ethers.getSigners();
-  console.log(
-    "Deploying the contracts with the account:",
-    await deployer.getAddress()
-  );
-  console.log("Account balance:", (await deployer.getBalance()).toString());
-
-  await deployL2Contracts();
-}
-
-// L2 Contracts
-
-async function deployL2Contracts() {
+export async function deployL2Contracts() {
   // Nitro Contracts
   const _inbox = "0x0A0dD8845C0064f03728F7f145B7DDA05FD0Ccc6";
 
@@ -60,10 +46,3 @@ async function deployL2Contracts() {
   console.log(">>> L2LiquidityGateway: ", l2LiquidityGateway.address);
   console.log("---------------------------------------------");
 }
-
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
